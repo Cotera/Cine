afab323f085f9820a2ec0388682810cc8c94b46c
cursoCIC00 <cursoCIC00@outlook.com>
2017-08-29 18:23:33 +0200

    ...

---
+++ CineMaestro_v2/js/components/entrada.vue
@@ -0,0 +1,37 @@
+
+Vue.component( 'entradas-header', {
+	template: `
+		<div class="header h-entrada">
+			<h1>
+			Entradas
+			</h1>
+		</div>
+	`
+});
+
+Vue.component( 'entradas-content', {
+	template: `
+		<div class="container">
+			<div class="row t-header">
+				<div class="col-md-3"> ID </div>
+				
+				<div class="col-md-3"> FILA </div>
+				
+				<div class="col-md-3"> BUTACA </div>
+				
+				<div class="col-md-3"> TITULAR </div>
+			</div>
+			
+			<div class="row t-body">
+				<div class="col-md-3"> </div>
+				
+				<div class="col-md-3"> </div>
+				
+				<div class="col-md-3"> </div>
+			
+				<div class="col-md-3"> </div>
+			</div>
+		</div>
+	`
+});
+
