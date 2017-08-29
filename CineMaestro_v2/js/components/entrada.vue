40dc72c438da64bdf56c1b9f272aa8c06765f67e
cursoCIC00 <cursoCIC00@outlook.com>
2017-08-29 18:28:10 +0200

    ...

--- CineMaestro_v2/js/components/entrada.vue
+++ CineMaestro_v2/js/components/entrada.vue
@@ -1,37 +1,36 @@
+<template>
+	<div class="header h-entrada">
+		<h1>
+		Entradas
+		</h1>
+	</div>
 
-Vue.component( 'entradas-header', {
-	template: `
-		<div class="header h-entrada">
-			<h1>
-			Entradas
-			</h1>
-		</div>
-	`
-});
-
-Vue.component( 'entradas-content', {
-	template: `
-		<div class="container">
-			<div class="row t-header">
-				<div class="col-md-3"> ID </div>
-				
-				<div class="col-md-3"> FILA </div>
-				
-				<div class="col-md-3"> BUTACA </div>
-				
-				<div class="col-md-3"> TITULAR </div>
-			</div>
+	<div class="container">
+		<div class="row t-header">
+			<div class="col-md-3"> ID </div>
 			
-			<div class="row t-body">
-				<div class="col-md-3"> </div>
-				
-				<div class="col-md-3"> </div>
-				
-				<div class="col-md-3"> </div>
+			<div class="col-md-3"> FILA </div>
 			
-				<div class="col-md-3"> </div>
-			</div>
+			<div class="col-md-3"> BUTACA </div>
+			
+			<div class="col-md-3"> TITULAR </div>
 		</div>
-	`
-});
+		
+		<div class="row t-body">
+			<div class="col-md-3"> </div>
+			
+			<div class="col-md-3"> </div>
+			
+			<div class="col-md-3"> </div>
+		
+			<div class="col-md-3"> </div>
+		</div>
+	</div>
+	
+</template>
 
+<script>
+	export default{
+		name: 'entrada'
+	}
+</script>
